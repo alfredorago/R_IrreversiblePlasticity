c63b471686df80f30d9fb83928bfad2d640ec98d
AlfredoRago <alfredo.rago@gmail.com>
2019-05-13 12:36:02 +0200

    Development for univariate cue and trait implemented in R
    Still need to implement constrained positive gene concentrations.

---
+++ Rscripts/Univariate_ReactionNorm.R
@@ -0,0 +1,35 @@
+#### Plot reaction norms from GRNs
+
+### Import library and define functions
+library(tidyr)
+activation <- function(x){
+  y <- tanh(x)
+  y <- (y+1)/2
+  y
+}
+
+### Define development
+develop <- function(cues, epigen, grn, devtime, mzmat){
+  # For every cue in our range, iterate hidden layer
+  sapply(cues, function(c){
+    for (i in 1:devtime) {
+      epigen[1] <- (c + epigen[1])/2
+      epigen <- epigen %*% grn
+      epigen <- activation(epigen)
+    }
+    epigen
+  }
+  ) %>% 
+    # Multiply final hidden layer states by output matrix
+    apply(MARGIN = 2, function(x){mzmat %*% x}) %>%
+    # convert to data.frame with rows named after inputs 
+  matrix() %>% data.frame(row.names = round(cues, digits = 2))
+}
+
+### Functional test for development with pre-set parameters
+devtime1 <- 20
+cues1 <- seq(1, -1, length.out = 10)/2
+grn1 <- matrix(nrow = 4, data = c(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1))
+epigen1 <- rep(0.5, ncol(grn1))
+mzmat <- matrix(data = rep(1, nrow(grn1)), nrow = 1)
+develop(cues1, epigen1, grn1, devtime1, mzmat)
