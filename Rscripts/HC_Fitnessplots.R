5d3e0afb15e60f182c9d5a2e497cb7c0f8740747
Unknown <alfredo.rago@gmail.com>
2019-05-21 18:48:46 +0200

    Implement plots for fitness over time
    Working perfectly

--- Rscripts/HC_Fitnessplots.R
+++ Rscripts/HC_Fitnessplots.R
@@ -20,14 +20,35 @@ trainPhenoList <- lapply(
   header = FALSE,
   col.names = c("Replicate", "Generation", "Individual", "Environment", "Trait", "Phenotype", "Fitness")
 )
-names(trainPhenoList) <- str_extract(string = as.character(trainPheno), pattern = "(?<=[//])[A-C]0")
+names(trainPhenoList) <- str_extract(string = as.character(trainPheno), pattern = "(?<=[//])[A-Z]0")
 trainPhenoData <- ldply(trainPhenoList)
 trainPhenoData$training <- factor(x = rep(1, nrow(trainPhenoData)), levels = c(0,1))
 trainPhenoData$problem1 <- factor(x = str_extract(string = trainPhenoData$.id, pattern = "^."))
 trainPhenoData$problem2 <- factor(x = str_extract(string = trainPhenoData$.id, pattern = ".$"), levels = c(0,"A","B") )
+
 ## Import and annotate phenotype files from test simulations
+testDir <- dir(path = simulDir, pattern = "[^0]$", full.names = T)
+testPheno <- list.files(path = testDir, pattern = "PHEN.*", full.names = T)
+testPhenoList <- lapply(
+  X = testPheno, 
+  FUN = fread, 
+  header = FALSE,
+  col.names = c("Replicate", "Generation", "Individual", "Environment", "Trait", "Phenotype", "Fitness")
+)
+names(testPhenoList) <- str_extract(string = as.character(testPheno), pattern = "(?<=[//])[A-Z]{2}")
+testPhenoData <- ldply(testPhenoList)
+testPhenoData$training <- factor(x = rep(0, nrow(trainPhenoData)), levels = c(0,1))
+testPhenoData$problem1 <- factor(x = str_extract(string = testPhenoData$.id, pattern = "^."))
+testPhenoData$problem2 <- factor(x = str_extract(string = testPhenoData$.id, pattern = ".$"), levels = c(0,"A","B") )
+# Warning, code below assumes same number of generations for each test run
+testPhenoData$Generation <- testPhenoData$Generation+max(trainPhenoData$Generation)
 
 ## Merge files
-
+PhenoData <- rbind(trainPhenoData, testPhenoData)
+PhenoData$Group <- factor(paste0(PhenoData$.id, PhenoData$Replicate))
 
 ## Plot
+ggplot(data = PhenoData, 
+  mapping = aes(x = Generation, y = Fitness, col = problem2, group = Group)) +
+  geom_line() +
+  facet_grid(.~problem1)
